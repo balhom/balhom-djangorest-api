6f0d593767df0e9c8d9c5a55d533b83544d1c8d7
fabbo-repo <fscherle98@gmail.com>
2023-09-27 19:18:29 +0200

    Balance models

---
+++ api/src/balance/models/balance_type_model.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BalanceTypeChoices(models.TextChoices):
+    REVENUE = 'expense', _('expense')
+    EXPENSE = 'revenue', _('revenue')
+
+
+class BalanceType(models.Model):
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=15
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=BalanceTypeChoices.choices
+    )
+    image = models.ImageField(
+        verbose_name=_("image"),
+        upload_to="balance/type",
+        default="core/default_image.jpg"
+    )
+
+    class Meta:
+        verbose_name = _("Balance type")
+        verbose_name_plural = _("Balance types")
+        unique_together = (('name', 'type'),)
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return str(self.name)
