67208ebb4347840218f874d275b37e26a453acd6
fabbo-repo <fscherle98@gmail.com>
2023-12-29 22:35:31 +0100

    Pagination changed from 10 to 15

--- src/core/settings.py
+++ src/core/settings.py
@@ -200,7 +200,7 @@ class Dev(Configuration):
             "rest_framework.permissions.IsAuthenticated",
         ],
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 10,
+        "PAGE_SIZE": 15,
         "DEFAULT_FILTER_BACKENDS": [
             "django_filters.rest_framework.DjangoFilterBackend",
         ],
